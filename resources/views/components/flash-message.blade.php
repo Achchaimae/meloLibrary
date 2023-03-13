1e3747982a6d2798e7b70467edb890d2e542ee2f
achchaimae <achchaimae1@gmail.com>
2023-03-13 19:28:03 +0100

    flash message 👌

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,7 @@
+@if (session()->has('message'))
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false , 3000)" x-show="show" class="fixed z-20 top-0 left-1/2 -translate-x-1/2 transform bg-blue-400 text-black font-semibold px-48 py-3 rounded-b-xl">
+        <p>
+            {{session('message')}}
+        </p>
+    </div>
+@endif

d7ab30663730f7c478f0de851080e05761080088
achchaimae <achchaimae1@gmail.com>
2023-03-13 19:40:33 +0100

    migrattion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nickname')->unique();
             $table->string('password');
+            $table->date('birthday');
+            $table->string('gender');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

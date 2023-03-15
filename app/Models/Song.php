b0180b6c435d3176e7a76ae7d4e49e03d71d78a7
achchaimae <achchaimae1@gmail.com>
2023-03-15 18:41:42 +0100

    song model ❄️

---
+++ app/Models/Song.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+
+{
+
+    use HasFactory;
+    protected $table = 'songs';
+    protected $fillable = [
+        'title',
+        'artist',
+        'writer',
+        'tag',
+        'language',
+        'release_date',
+        'duration',
+        'lyrics',
+    ];
+}

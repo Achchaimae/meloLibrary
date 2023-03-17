0104a41a83b51eb43f0e9131f5fb1471e1597101
achchaimae <achchaimae1@gmail.com>
2023-03-17 11:17:15 +0100

    music js done 🐬

--- public/js/handleMusic.js
+++ public/js/handleMusic.js
@@ -1,13 +1,12 @@
-
 // all music on table
 let music = document.querySelectorAll('.musicTable');
-
 
 // music footer bar
 let footer = document.getElementById('footer');
 let footerImg = document.getElementById('footerImg');
 let footerTitle = document.getElementById('footerTitle');
 let footerAlbum = document.getElementById('footerAlbum');
+// footer audio src
 let footerMusic = document.getElementById('footerMusic');
 // footer play button
 let playBtn = document.getElementById('play');
@@ -17,17 +16,33 @@ let pauseBtn = document.getElementById("pause");
 let nextBtn = document.getElementById("next");
 // footer previous btn
 let previousBtn = document.getElementById("previous");
+// footer volum
+let volumInp = document.getElementById('volumInp');
+// footer mute btn
+let muteBtn = document.getElementById('mute');
+// footer demute btn
+let unmuteBtn = document.getElementById('unmute');
+// footer start music duration
+let startMusicDuration = document.getElementById("startMusicDuration");
+// footer end music duration
+let endMusicDuration = document.getElementById("endMusicDuration");
+// footer progress bar
+let progressBar = document.getElementById("progressBar");
+
 
 let currentSong = '';
 let indexOfSong = 0;
+let currentDuration = 0;
+volumInp.value = 80;
+progressBar.value = 0;
 
 music.forEach((ele, i) => {
-    ///////
-    ele.addEventListener("click", () => {
+    ele.addEventListener("dblclick", () => {
         // remove bg from all element
         music.forEach((ele) => {
             ele.classList.remove("bg-gray-200/20");
         });
+
         // add bg to the selected element
         ele.classList.add("bg-gray-200/20");
         // current song that clicked on
@@ -44,13 +59,34 @@ music.forEach((ele, i) => {
 
         // play music on the footer bar
         footerMusic.play();
-        console.log(currentSong);
+        console.log(footerMusic.currentTime);
+        // give the footer current volume
+        footerMusic.volume = volumInp.value / 100;
 
         playBtn.classList.add("hidden");
         pauseBtn.classList.remove("hidden");
-        console.log(indexOfSong);
     });
 })
+
+// get the duration of a song
+footerMusic.addEventListener('loadedmetadata', () => {
+    currentDuration = footerMusic.duration;
+    let second = `${parseInt(`${currentDuration % 60}`, 10)}`.padStart(2,'0');
+    let minute = parseInt(`${(currentDuration / 60) % 60}`, 10);
+    endMusicDuration.textContent = `${minute}:${second}`;
+    progressBar.max = currentDuration;
+});
+// get updated time by second and update the progress bar in the same time
+footerMusic.addEventListener('timeupdate', () => {
+    updateProgress(footerMusic.currentTime);
+});
+// update progressbar while music played
+function updateProgress(time) {
+    progressBar.value = time;
+    let second = `${parseInt(`${time % 60}`, 10)}`.padStart(2, "0");
+    let minute = parseInt(`${(time / 60) % 60}`, 10);
+    startMusicDuration.textContent = `${minute}:${second}`;
+} 
 
 // play music 
 playBtn.addEventListener("click", () => {
@@ -119,4 +155,22 @@ function handleFooter(img, title, album, audio) {
     footerMusic.play();
 }
 
+// handle the volum
+volumInp.addEventListener('input', () => {
+    mute.classList.remove("text-gray-500");
+    if (volumInp.value <= 1) {
+        mute.classList.add("text-gray-500");
+    }
+    footerMusic.volume = volumInp.value / 100;
+})
 
+mute.addEventListener('click', () => {
+    mute.classList.add('text-gray-500');
+    volumInp.value = 0;
+    footerMusic.volume = 0;
+})
+unmute.addEventListener('click', () => {
+    mute.classList.remove("text-gray-500");
+    volumInp.value = 100;
+    footerMusic.volume = volumInp.value / 100;
+})

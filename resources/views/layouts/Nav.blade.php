9d2b03397e8aaacf7c156fc9124eb1faa3363beb
achchaimae <achchaimae1@gmail.com>
2023-03-17 11:18:10 +0100

    styling 🎨

--- resources/views/layouts/Nav.blade.php
+++ resources/views/layouts/Nav.blade.php
@@ -3,31 +3,37 @@
     <div class="px-5 py-3">
         <img src="./images/LM9AWED.png" class="w-10 h-10 rounded-full" alt="">
     </div>
-    <ul class="px-5 flex h-96 flex-col place-content-evenly">
+    <ul class="w-full flex h-96 flex-col place-content-evenly">
 
-            <a href="/" class="hover:bg-white hover:text-blue-500 p-2 ">
+            <a href="/" class="hover:bg-white hover:text-blue-500 py-2 relative top-20">
                 <li>
-                    <i class="fa-solid fa-house"></i>
+                    <i class="fa-solid fa-house px-5"></i>
                     <span class="hidden sm:inline"> Explore</span>
                 </li>
             </a>
-            <a href="artist" class="hover:bg-white hover:text-blue-500 p-2 ">
+            <a href="artist" class="hover:bg-white hover:text-blue-500 py-2 relative top-20">
                 <li>
-                    <i class="fa-solid fa-user"></i>
+                    <i class="fa-solid fa-user px-5"></i>
                     <span class="hidden sm:inline">Artists</span>
                 
                 </li>
             </a>
-            <a href="album" class="hover:bg-white hover:text-blue-500 p-2 ">
+            <a href="album" class="hover:bg-white hover:text-blue-500 py-2 relative top-20">
                 <li>
-                    <i class="fa-solid fa-compact-disc"></i>
+                    <i class="fa-solid fa-compact-disc px-5"></i>
                     <span class="hidden sm:inline">Albums</span>
                 </li>
             </a>
-            <a href="playlist" class="hover:bg-white hover:text-blue-500 p-2 " >
+            <a href="playlist" class="hover:bg-white hover:text-blue-500 py-2 relative top-20" >
                 <li>
-                    <i class="fa-solid fa-music"></i>
+                    <i class="fa-solid fa-music px-5"></i>
                     <span class="hidden sm:inline">Playlist</span>
+                </li>
+            </a>
+            <a href="/logout" class="hover:bg-white hover:text-blue-500 py-2 relative top-72">
+                <li>
+                    <i class="fa-solid fa-sign-out px-5"></i>
+                    Logout
                 </li>
             </a>
         

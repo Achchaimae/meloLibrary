ddc124e4639b81b773057a7e3bac1f9d9fe973e0
achchaimae <achchaimae1@gmail.com>
2023-03-23 17:27:21 +0000

    updated

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,8 +10,20 @@ class PagesController extends Controller
     public function index() {
         return view('index');
     }
-    public function album() {
-        $songs = DB::table('songs')->get();
-        return view('Album', ['songs' => $songs]);
-    }
+    // public function album() {
+    //     $songs = DB::table('songs')->get();
+    //     $playlists = DB::table('playlists')->get();
+    //     return view('album', ['songs' => $songs, 'playlists' => $playlists]);
+    // }
+    public function album()
+{
+    $songs = DB::table('songs')->get();
+    $playlists = DB::table('playlists')->get();
+    return view('album', compact('songs', 'playlists'));
+    
+}
+
+    
+    
+
 }
